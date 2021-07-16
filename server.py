292c30edac09d4a6c1e0738f59a637644994d254
Andrei Itiligator <maintainer@80-78-255-63.cloudvps.regruhosting.ru>
2021-07-16 06:45:43 +0300

    feature: logs response to GET

--- server.py
+++ server.py
@@ -16,7 +16,9 @@ class S(BaseHTTPRequestHandler):
     def do_GET(self):
         logging.info("GET request,\nPath:%s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        # self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        with open("importer.log", "r") as log:
+            self.wfile.write(log.read().encode('utf-8'))
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
